e70d2cda247af2460745c7572197aaa840e7d18d
Z0XXXy <potyakin.a.ssau@gmail.com>
2021-09-10 15:28:00 +0400

    Добавьте файлы проекта.

---
+++ TPlaba1/TPlaba1.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include "MathLibrary.h"
+#include <vector>
+
+int main()
+{
+    std::vector<int> v;
+    int n,x;
+    std::cout << "Enter a vector lenght:";
+    std::cin >> n;
+    for (int i=0; i < n; i++) {
+        std::cout << "Enter " << i << " number:";
+        std::cin >> x;
+        v.push_back(x);
+    }
+    std::cout <<"Result:"<< number_of_numbers(v);
+}
