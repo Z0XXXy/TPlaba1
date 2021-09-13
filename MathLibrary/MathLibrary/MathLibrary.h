c604563526373717949f9c772febde5d8ef3f615
Z0XXXy <87202299+Z0XXXy@users.noreply.github.com>
2021-09-13 19:40:03 +0400

    Delete MathLibrary directory

--- MathLibrary/MathLibrary/MathLibrary.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <vector>
-#ifdef MATHLIBRARY_EXPORTS
-#define MATHLIBRARY_API __declspec(dllexport)
-#else
-#define MATHLIBRARY_API __declspec(dllimport)
-#endif
-
-extern "C" MATHLIBRARY_API unsigned long long number_of_numbers(std::vector<int> v);
